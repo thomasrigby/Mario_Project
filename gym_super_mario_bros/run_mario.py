36845aab58033414c8a0699f7f71634fe4080f22
thomas rigby <tomr0607@hotmail.com>
2023-09-09 18:12:25 +0800

    hi filip

--- gym_super_mario_bros/run_mario.py
+++ gym_super_mario_bros/run_mario.py
@@ -3,9 +3,9 @@ import numpy as np
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-import matplotlib.pyplot as plt
 import gym
 import time
+import os 
 
 # Initialize environment
 env = gym.make('SuperMarioBros-v0', apply_api_compatibility=True, render_mode="human")
@@ -14,35 +14,126 @@ env = JoypadSpace(env, SIMPLE_MOVEMENT)
 # Define actions
 RIGHT = 1
 RIGHT_JUMP = 2
-RIGHT_SPEED_JUMP = 4
+
 
 # Custom jump action (hold 'A' button for longer)
-#This is hard coded atm (change number that is * to see speed)
-CUSTOM_JUMP = [4] * 23 # Hold 'A' button for 27 frames
+CUSTOM_JUMP = [4] * 23  # Hold 'A' button for 27 frames
+
+# # # Create a window for trackbars
+# cv2.namedWindow('Trackbars')
+
+# # Function to update Koopa color range using trackbars
+# def update_koopa_color_range(*args):
+#     global lower_bound_koopa, upper_bound_koopa
+#     lower_bound_koopa = np.array([
+#         cv2.getTrackbarPos('Lower H', 'Trackbars'),
+#         cv2.getTrackbarPos('Lower S', 'Trackbars'),
+#         cv2.getTrackbarPos('Lower V', 'Trackbars')
+#     ])
+#     upper_bound_koopa = np.array([
+#         cv2.getTrackbarPos('Upper H', 'Trackbars'),
+#         cv2.getTrackbarPos('Upper S', 'Trackbars'),
+#         cv2.getTrackbarPos('Upper V', 'Trackbars')
+#     ])
+#     display_koopa_mask()
+
+# def display_koopa_mask():
+#     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+#     mask_koopa = cv2.inRange(hsv, lower_bound_koopa, upper_bound_koopa)
+#     result = cv2.bitwise_and(frame, frame, mask=mask_koopa)
+#     cv2.imshow('Koopa Color Mask', result)
+
+# # Create trackbars for Koopa color range
+# cv2.createTrackbar('Lower H', 'Trackbars', 0, 179, update_koopa_color_range)
+# cv2.createTrackbar('Lower S', 'Trackbars', 0, 255, update_koopa_color_range)
+# cv2.createTrackbar('Lower V', 'Trackbars', 0, 255, update_koopa_color_range)
+# cv2.createTrackbar('Upper H', 'Trackbars', 179, 179, update_koopa_color_range)
+# cv2.createTrackbar('Upper S', 'Trackbars', 255, 255, update_koopa_color_range)
+# cv2.createTrackbar('Upper V', 'Trackbars', 255, 255, update_koopa_color_range)
+
+# # Load the frame from the frames directory
+# frame_path = "frames/frame_80.png"
+# frame = cv2.imread(frame_path)
+
+# # Check if the frame was loaded correctly
+# if frame is not None:
+#     # Display the frame and initial mask
+#     cv2.imshow('Loaded Frame', frame)
+#     display_koopa_mask()
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+# else:
+#     print(f"Failed to load the frame from {frame_path}")
 
 # Goomba Color Range
-lower_bound_goomba = np.array([0, 237, 227])
-upper_bound_goomba = np.array([179, 255, 247])
+lower_bound_goomba = np.array([14, 0, 0])
+upper_bound_goomba = np.array([17, 184, 255])
 
 # Pipe Color Ranges (using the values you provided)
-lower_bound_pipe = np.array([38, 0, 0])
-upper_bound_pipe = np.array([41, 230, 255])
+lower_bound_pipe = np.array([12, 196, 0])
+upper_bound_pipe = np.array([179, 254, 255])
 
 # Ground Color Range (you may need to adjust these values)
 lower_bound_ground = np.array([0, 237, 227])
 upper_bound_ground = np.array([179, 255, 247])
 
-INITIAL_DELAY = 10  # Added constant for initial delay
+# Koopa Color Range (using the values you provided)
+lower_bound_koopa = np.array([0, 0, 0])
+upper_bound_koopa = np.array([0, 0, 255])
+
+def analyze_koopa_aspect_ratio(frame_path):
+    frame = cv2.imread(frame_path)
+
+    if frame is None:
+        print(f"Failed to load the frame from {frame_path}")
+        return
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask_koopa = cv2.inRange(hsv, lower_bound_koopa, upper_bound_koopa)
+
+    # Find contours in the Koopa mask
+    contours, _ = cv2.findContours(mask_koopa, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # Analyze aspect ratios
+    for contour in contours:
+        if cv2.contourArea(contour) > 50:  # Only consider significant contours
+            x, y, w, h = cv2.boundingRect(contour)
+            aspect_ratio = w / h
+            print(f"Aspect Ratio of detected Koopa: {aspect_ratio}")
+
+            # Just for visualization
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    # Display the frame with detected Koopas
+    cv2.imshow('Koopa Detection', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+INITIAL_DELAY = 20  # Added constant for initial delay
 
 last_x_position = None
 obstacle_detected = False  # Flag to keep track of obstacle detection
 no_ground_detected = False  # Flag to keep track of no ground detection
 
+# frame_counter = 0
+# frame_interval = 10  # Save a frame every 100 steps
+# frame_save_path = "frames"  # Directory to save frames
+
+# # Create the frames directory if it doesn't exist
+# os.makedirs(frame_save_path, exist_ok=True)
+
+# def save_frame(obs):
+#     global frame_counter
+#     if frame_counter % frame_interval == 0:
+#         frame_save_name = os.path.join(frame_save_path, f"frame_{frame_counter}.png")
+#         cv2.imwrite(frame_save_name, cv2.cvtColor(obs, cv2.COLOR_RGB2BGR))
+#     frame_counter += 1
+
 def detect_obstacle(frame, mario_x_position):
     global last_x_position, obstacle_detected
     # Adjusting the region of interest
     region_start_percentage = 0.3
-    region_end_percentage = 0.7
+    region_end_percentage = 0.8
     vertical_start_percentage = 0.5  # Adjusted to avoid sky boxes
     vertical_end_percentage = 0.9
 
@@ -61,33 +152,48 @@ def detect_obstacle(frame, mario_x_position):
     # Pipe Masks
     mask_pipe = cv2.inRange(hsv_region, lower_bound_pipe, upper_bound_pipe)
 
-    # Combine Goomba and Pipe masks
-    mask_combined = cv2.bitwise_or(mask_goomba, mask_pipe)
+    # Koopa Mask
+    mask_koopa = cv2.inRange(hsv_region, lower_bound_koopa, upper_bound_koopa)
+
+    # Combine Goomba, Pipe, and Koopa masks
+    mask_combined = cv2.bitwise_or(cv2.bitwise_or(mask_goomba, mask_pipe), mask_koopa)
 
     # Debugging - Display the combined mask
     cv2.imshow('Obstacle Detection Mask', mask_combined)
     cv2.waitKey(1)
 
-    # Check for contours to differentiate between Goomba and Pipe
-    contours, _ = cv2.findContours(mask_goomba, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Check for contours
+    contours, _ = cv2.findContours(mask_combined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     obstacles_detected = []
 
+
+
     for contour in contours:
         if cv2.contourArea(contour) > 50:
+            
             x, y, w, h = cv2.boundingRect(contour)
             aspect_ratio = w/h
-
+            
             obstacles_detected.append((aspect_ratio, h))
-
+            
+        
             # Check aspect ratio for Goomba
-            if 1.6 <= aspect_ratio <= 1.8:  # Slightly broadened for safety
+            if 1.7 <= aspect_ratio <= 1.8:
+                print("Goomba detected!")
                 return "goomba"
 
-            # Check if the aspect ratio is 1.88 (pipe)
-            if aspect_ratio == 1.88 and not obstacle_detected:
-                obstacle_detected = True
+            # Check aspect ratio for Pipe
+            if 1.8 <= aspect_ratio <= 2.8:  # Adjusted the value slightly for safety
+                print("Pipe detected!")
+                print(aspect_ratio)
                 return "pipe"
+
+            # Check aspect ratio for Koopa (assuming it's around 1.3 for this example)
+            if 1.44 <= aspect_ratio <= 1.46:
+                print("Koopa detected!")
+                return "koopa"
+
 
     # Reset the obstacle detection flag when Mario moves
     if mario_x_position != last_x_position:
@@ -95,8 +201,8 @@ def detect_obstacle(frame, mario_x_position):
 
     return obstacles_detected
 
-#this needs fixing 
-#will check colour of absent ground and jump before with mask
+# This needs fixing 
+# Will check color of absent ground and jump before with mask
 def detect_ground(frame):
     # 1. Identify the Sky Color:
     sky_row = 10  # Y-coordinate for a row that's always in the sky
@@ -128,27 +234,10 @@ for step in range(5000):
     else:
         no_ground_detected = False
     
-    if last_x_position is not None and last_x_position == mario_x_position:
-        stopped_frames += 1
 
-        # If Mario hasn't moved for more than 10 frames, analyze obstacle
-        if stopped_frames >= 10:
-            obstacles_detected = detect_obstacle(obs, mario_x_position)
-            if isinstance(obstacles_detected, list):
-                for aspect_ratio, height in obstacles_detected:
-                    if aspect_ratio == 1.88:  # Check if "pipe" is detected
-                        # Perform a custom jump
-                        for jump_frame in CUSTOM_JUMP:
-                            obs, reward, terminated, truncated, info = env.step(RIGHT_JUMP)
-                    print(f"Obstacle detected when Mario stopped! Aspect Ratio: {aspect_ratio:.2f} Height: {height}")
-
-    else:
-        stopped_frames = 0
-
-    last_x_position = mario_x_position  # Update the last position
-    
     obstacle = detect_obstacle(obs, mario_x_position)
-    if obstacle == "goomba" or obstacle == "pipe":
+
+    if obstacle == "goomba" or obstacle == "pipe" or obstacle == "koopa":
         # Perform a custom jump
         for jump_frame in CUSTOM_JUMP:
             obs, reward, terminated, truncated, info = env.step(RIGHT_JUMP)
@@ -160,6 +249,9 @@ for step in range(5000):
     if done:
         env.reset()
 
-    time.sleep(0.01)
+    time.sleep(0.001)
+
+    # Capture and save frames
+    #save_frame(obs)
 
 env.close()
